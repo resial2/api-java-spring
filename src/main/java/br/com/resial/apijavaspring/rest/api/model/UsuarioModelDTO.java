c3aabd3050ab0ff662ba3091d3faf81b27b39bce
João Rener <joao.rener42@gmail.com>
2022-09-04 20:27:44 -0300

    Adicionada classe DTO

---
+++ src/main/java/br/com/resial/apijavaspring/rest/api/model/UsuarioModelDTO.java
@@ -0,0 +1,42 @@
+package br.com.resial.apijavaspring.rest.api.model;
+
+public class UsuarioModelDTO {
+
+    private Integer id;
+    private String nome;
+    private String login;
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
